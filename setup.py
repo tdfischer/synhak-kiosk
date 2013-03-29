1a8e5c65496f026dae277e2b8c54d8557543eaac
Trever Fischer <tdfischer@phrobo.net>
2013-03-29 16:18:12 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from disutuils.core import setup
+
+setup(name='Bonehead',
+  version'0.1.0',
+  description='An interactive kiosk framework for hackerspaces',
+  author='Trever Fischer',
+  author_email='tdfischer@phrobo.net',
+  url='http://spacekit.phrobo.net/',
+  py_modules=['bonehead'],
+  scripts=['bonehead-ui'],
+)
